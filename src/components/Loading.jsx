bcb95a20d6310b29cb1a33000b4af79e10220613
mthnnn <muratdemiryurek7@gmail.com>
2025-05-20 12:03:28 +0300

    güncl

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,6 @@
 export default function Loading() {
   return (
+    // burada loadin fonksiyonunu yapıyoruz
     <div className="spinner-border text-primary" role="status">
       <span className="visually-hidden">Loading...</span>
     </div>
