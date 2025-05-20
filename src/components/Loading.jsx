842767be3fed3ca36655d1fedd29fbf9b1af6558
mthnnn <muratdemiryurek7@gmail.com>
2025-05-20 12:07:27 +0300

    değişiklik kaldırıldı

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    // burada loadin fonksiyonunu yapıyoruz
+    // burada loadin fonksiyonunu yapılıyor
     <div className="spinner-border text-primary" role="status">
       <span className="visually-hidden">Loading...</span>
     </div>
