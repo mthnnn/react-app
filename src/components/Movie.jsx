6dd84c764b11fb8445baf1885f4bb84181b6143b
mthnnn <muratdemiryurek7@gmail.com>
2025-05-25 13:08:21 +0300

    C

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,23 +1,19 @@
-export default function Movie({ movieObj, onAddToList, onSelectedMovie }) {
+import { Link } from "react-router";
+
+export default function Movie({ movieObj }) {
   return (
     <div className="col">
-      <div
-        className="card movie position-relative"
-        onClick={() => onSelectedMovie(movieObj)}
-      >
-        <img
-          src={"https://image.tmdb.org/t/p/original/" + movieObj.poster_path}
-          alt=""
-          className="card-img-top"
-        />
+      <div className="card movie position-relative h-100">
+        <Link to={`/movies/${movieObj.id}`}>
+          <img
+            src={"https://image.tmdb.org/t/p/original/" + movieObj.poster_path}
+            alt=""
+            className="card-img-top"
+          />
+        </Link>
+
         <div className="card-body">
           <h2 className="h6 card-title">{movieObj.title}</h2>
-          <button
-            className="btn btn-link fs-5 text-danger position-absolute top-0 start-0"
-            onClick={() => onAddToList(movieObj)}
-          >
-            <i className="bi bi-plus-circle"></i>
-          </button>
         </div>
       </div>
     </div>
