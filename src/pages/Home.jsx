3ba21623318a85239383f21987f8bd8772bc310e
mthnnn <muratdemiryurek7@gmail.com>
2025-05-26 15:35:13 +0300

    t

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
           <div className="container pt-5">
             <div className="row">
               <div className="col-12 col-lg-7 mx-auto text-center text-white">
-                <h1 className="display-2">Hoş Geldin Müjde kuju!</h1>
+                <h1 className="display-2">Hoş Geldiniz!</h1>
                 <p className="lead">
                   Keşfedilecek milyonlarca film, TV şovu ve kişi. Şimdi
                   keşfedin.
